953b70008f0000867bf6d786f23bc0327b16ad5e
lxk0301 <778732665@qq.com>
2018-11-22 18:25:10 +0800

    add

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API2: '"https://www.eleclifecycle.com"',
-  BASE_API: '"http://localhost:8080/hzmis"',
+  BASE_API: '"https://www.eleclifecycle.com"',
+  BASE_API2: '"http://localhost:8080/hzmis"',
   BASE_FILE_API: '"http://localhost:8080/hzmis/c/common/fileRecord/uploadFile/"'
 }
